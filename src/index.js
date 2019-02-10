ba77c1673ef65f17136d8e94b652571f7767dd0e
marcin-pasiewicz <marcin.pasiewicz@gmail.com>
2019-02-10 20:18:40 +0100

    add users query

--- src/index.js
+++ src/index.js
@@ -10,20 +10,32 @@ const server = createServer();
 server.express.use(cookieParser());
 server.express.use((req, res, next) => {
     const {token} = req.cookies;
-    if(token) {
+    if (token) {
         const {userId} = jwt.verify(token, process.env.APP_SECRET)
         req.userId = userId
     }
     next()
 });
+server.express.use(async (req, res, next) => {
+    if (!req.userId) return next();
+    const user = await db.query.user({
+            where: {
+                id: req.userId
+            },
+        },
+        '{id, permissions, email, name}'
+    );
+    req.user = user;
+    next()
+});
 
 server.start({
-    cors: {
-        credentials: true,
-        origin: process.env.FRONTEND_URL,
-    }
-},
+        cors: {
+            credentials: true,
+            origin: process.env.FRONTEND_URL,
+        }
+    },
     deets => {
-    console.log(`Server is running on port http:localhost:${deets.port}`)
+        console.log(`Server is running on port http:localhost:${deets.port}`)
     }
 );
